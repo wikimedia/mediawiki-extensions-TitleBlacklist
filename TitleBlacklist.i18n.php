8d6585ad059e12ce720c339dbe1e3132b5d1baee
Raimond Spekking <raymond@users.mediawiki.org>
2007-11-05 22:58:19 +0000

    * Updates German

--- TitleBlacklist.i18n.php
+++ TitleBlacklist.i18n.php
@@ -11,6 +11,14 @@ function efGetTitleBlacklistMessages()
 ",
 			'titleblacklist-forbidden' => "'''This page title is blacklisted.''' It matches following blacklist regex: '''''\$1'''''",
 		),
+		'de' => array(
+			'titleblacklist' =>
+"# Dies ist die Schwarze Liste unerwünschter Seitennamen.
+# Jeder Seitenname, auf den die folgenden regulären Ausdrücke zutreffen, kann nicht erstellt und bearbeitet werden.
+# Text hinter einer Raute „#“ wird als Kommentar gesehen
+",
+			'titleblacklist-forbidden' => "'''Dieser Seitenname steht auf der Schwarzen Liste.''' Der folgende reguläre Ausdruck traf zu: '''''\$1'''''",
+		),
 		'nl' => array(
 			'titleblacklist' => '# Dit is een zwarte lijst voor paginanamen
 # Iedere paginanaam die voldoet aan de reguliere expressie kan niet aangemaakt en bewerkt worden
