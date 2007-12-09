90dafd679729c4592d650e9c247f8b27e2e664ac
Siebrand Mazeland <siebrand@users.mediawiki.org>
2007-12-09 22:48:21 +0000

    Localisation updates from Betawiki. * Fixes and additions to 18 extensions for ar, bg, br, fr, hsb, la, nl, ro

--- TitleBlacklist.i18n.php
+++ TitleBlacklist.i18n.php
@@ -28,10 +28,10 @@ It matches the following blacklist regex: '''''\$1'''''",
 # كل عنوان يطابق تعبيرا منتظما هنا ممنوع إنشاؤه وتعديله
 # استخدم "#" للتعليقات',
 			'titleblacklist-forbidden-move' => '<span class="error">
-\'\'\'الصفحة المعنونة "$2" لا يمكن نقلها إلى "$3"\'\'\' <br/>
+\'\'\'الصفحة المعنونة "$2" لا يمكن نقلها إلى "$3"\'\'\' <br />
 هي تطابق ريجيكس القائمة السوداء التالي: \'\'\'\'\'$1\'\'\'\'\'
 </span>',
-			'titleblacklist-forbidden-upload' => '\'\'\'الملف بالاسم "$2" لا يمكن رفعه\'\'\' <br/>
+			'titleblacklist-forbidden-upload' => '\'\'\'الملف بالاسم "$2" لا يمكن رفعه\'\'\' <br />
 هو يطابق ريجيكس القائمة السوداء التالي: \'\'\'\'\'$1\'\'\'\'\'',
 		),
 
@@ -60,23 +60,41 @@ It matches the following blacklist regex: '''''\$1'''''",
 # Kóžde nastawkowe mjeno, kotrež so k regularnemu wurazej hodźi, njesmě so wutworjeć a wobdźěłować
 # Wužij "#" za komentary',
 			'titleblacklist-forbidden-move' => '<span class="error">
-\'\'\'Strona z titulom "$2" njeda so do "$3"\'\'\' přesunyć.<br/>
+\'\'\'Strona z titulom "$2" njeda so do "$3"\'\'\' přesunyć.<br />
 Kryje so ze slědowacym regularnym wurazom čorneje lisćiny: \'\'\'\'\'$1\'\'\'\'\'
 </span>',
-			'titleblacklist-forbidden-upload' => '\'\'\'Dataja z mjenom "$2" njeda so nahrać\'\'\' <br/>
+			'titleblacklist-forbidden-upload' => '\'\'\'Dataja z mjenom "$2" njeda so nahrać\'\'\' <br />
 Kryje so ze slědowacym regularnym wurazom čorneje lisćiny: \'\'\'\'\'$1\'\'\'\'\'',
 		),
 
 		'la' => array(
 			'titleblacklist' => '# Hic est index titulorum prohibitorum
-# Tituli qui congruunt cum expressione regulari sequente ni creari ni recenseri possunt
+# Tituli qui congruunt cum expressione regulari sequente nec creari nec recenseri possunt
 # Utere "#" pro commentariis',
+			'titleblacklist-forbidden-edit' => '<div align="center" style="border: 1px solid #f88; padding: 0.5em; margin-bottom: 3px; font-size: 95%; width: auto;">
+\'\'\'Pagina cum titulo "$2" nec creari nec recenseri potest\'\'\' <br />
+Hic titulus congruit cum expressione regulari: \'\'\'\'\'$1\'\'\'\'\'
+</div>',
+			'titleblacklist-forbidden-move' => '<span class="error">
+\'\'\'Pagina cum titulo "$2" non ad "$3" moveri potest\'\'\' <br />
+Hic titulus congruit cum expressione regulari: \'\'\'\'\'$1\'\'\'\'\'
+</span>',
+			'titleblacklist-forbidden-upload' => '\'\'\'Fasciculus cum titulo "$2" onerari non potest\'\'\' <br />
+Hic titulus congruit cum expressione regulari: \'\'\'\'\'$1\'\'\'\'\'',
 		),
 
 		'nl' => array(
 			'titleblacklist' => '# Dit is een zwarte lijst voor paginanamen
 # Iedere paginanaam die voldoet aan de reguliere expressie kan niet aangemaakt en bewerkt worden
 # Gebruik "#" voor opmerkingen',
+			'titleblacklist-forbidden-edit' => '<div align="center" style="border: 1px solid #f88; padding: 0.5em; margin-bottom: 3px; font-size: 95%; width: auto;">
+\'\'\'Een pagina genaamd "$2" kan niet worden aangemaakt\'\'\' <br />
+Hij voldoet aan de volgende regex op de zwarte lijst: \'\'\'\'\'$1\'\'\'\'\'
+</div>',
+			'titleblacklist-forbidden-move' => '<span class="error">
+\'\'\'Een pagina genaamd "$2" kan niet worden hernoemd naar "$3"\'\'\' <br />
+Hij voldoet aan de volgende reguliere expressie op de zwarte lijst: \'\'\'\'\'$1\'\'\'\'\'
+</span>',
 		),
 	);
 
