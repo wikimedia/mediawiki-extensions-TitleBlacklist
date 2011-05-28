0a48a42539b54a7822ed91b98477b8af6e5be60f
Raimond Spekking <raymond@users.mediawiki.org>
2011-05-28 21:32:53 +0000

    Localisation updates for core and extension messages from translatewiki.net (2011-05-28 21:01:00 UTC)

--- TitleBlacklist.i18n.php
+++ TitleBlacklist.i18n.php
@@ -637,6 +637,7 @@ Dens la lista nêre, corrèspond a ceta entrâ : <code>$1</code>',
 Dens la lista nêre, corrèspond a ceta entrâ : <code>$1</code>',
 	'titleblacklist-invalid' => '{{PLURAL:$1|Ceta legne|Cetes legnes}} dens la lista nêre des titros {{PLURAL:$1|est envalida|sont envalides}} ;
 {{PLURAL:$1|la|les}} volyéd corregiér devant qu’encartar :',
+	'titleblacklist-override' => 'Ignorar la lista nêre',
 	'right-tboverride' => 'Ignorar la lista nêre des titros',
 );
 
